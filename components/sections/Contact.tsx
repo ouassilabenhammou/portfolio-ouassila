5af41967dbcc30b74d85a9124f7539294645ef94
ouassilabenhammou <509114@student.fontys.nl>
2026-05-01 16:00:42 +0200

    feat: voeg contact sectie toe

--- components/sections/Contact.tsx
+++ components/sections/Contact.tsx
@@ -1,5 +1,14 @@
 import React from "react";
-
+import SectieTitel from "../ui/SectieTitel";
+import ActionButton from "../ui/ActionButton";
 export default function Contact() {
-  return <div>Contact</div>;
+  return (
+    <section>
+      <SectieTitel
+        title="Laten we samen iets moois maken"
+        subtitle="Beschikbaar voor afstudeerstage"
+      />
+      <ActionButton imageSrc="/icons/arrow-button.svg" />
+    </section>
+  );
 }
