1374e66b2840e3534038ab0b75c4fe14ca98972d
ouassilabenhammou <509114@student.fontys.nl>
2026-05-01 20:32:48 +0200

    feat: pas positionering van naam

--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -2,8 +2,12 @@ import React from "react";
 
 export default function Hero() {
   return (
-    <section>
-      <img src="/images/naam.svg" alt="Hero-afbeelding" />
+    <section className="min-h-screen flex flex-col items-center justify-center">
+      <img
+        src="/images/naam.svg"
+        alt="Hero-afbeelding"
+        className="h-auto w-[406px] max-w-full md:w-auto md:max-w-none"
+      />
     </section>
   );
 }
