a076b4348a05c5c7f59cd292cb88f2669859c442
ouassilabenhammou <509114@student.fontys.nl>
2026-05-02 22:56:02 +0200

    feat: voeg over mij tekst toe

--- components/sections/OverMij.tsx
+++ components/sections/OverMij.tsx
@@ -1,38 +1,66 @@
-import React from "react";
 import HandIcon from "@/public/icons/hand.svg";
 import StudentCapIcon from "@/public/icons/student-cap.svg";
 import DevelopIcon from "@/public/icons/develop.svg";
 import LampIcon from "@/public/icons/lamp.svg";
 
+import { anton } from "@/app/fonts";
+
 export default function OverMij() {
   return (
-    <section>
-      HI{" "}
-      <HandIcon
-        aria-hidden
-        focusable="false"
-        className="inline-block align-middle mx-1 text-[#FF8FAB] [--icon-fill:#FFFDF5]"
-      />
-      Ik ben Ouassila een Associate Degree student{" "}
-      <StudentCapIcon
-        aria-hidden
-        focusable="false"
-        className="inline-block align-middle mx-1 text-[#6BC5D2]"
-      />
-      ICT met een achtergrond in Media Design. Momenteel verdiep ik me in
-      Front-end Development{" "}
-      <DevelopIcon
-        aria-hidden
-        focusable="false"
-        className="inline-block align-middle mx-1 text-[#4CAF78]"
-      />{" "}
-      en werk ik graag aan creatieve{" "}
-      <LampIcon
-        aria-hidden
-        focusable="false"
-        className="inline-block align-middle mx-1 text-[#FFD166]"
-      />
-      concepten die uitgroeien tot sterke digitale oplossingen.
+    <section
+      className={`${anton.className} flex flex-col items-center text-center uppercase xs:text-[28px] md:text-[45px] leading-[1.1] text-(--color-secondary) `}
+    >
+      <div>
+        {/* 1e regel */}
+        <div className="flex justify-center items-center text-center gap-2">
+          <p>HI</p>
+          <HandIcon
+            aria-hidden
+            focusable="false"
+            className="inline-block align-middle text-[#FF8FAB] [--icon-fill:#FFFDF5]"
+          />
+          <p>Ik ben Ouassila</p>
+        </div>
+
+        {/* 2e regel */}
+        <div className="flex flex-col items-center text-center">
+          <p>
+            een Associate Degree student
+            <StudentCapIcon
+              aria-hidden
+              focusable="false"
+              className="inline-block align-middle  text-[#6BC5D2]"
+            />
+            ICT met een achtergrond in
+          </p>
+        </div>
+        <div>
+          <p>Media Design. Momenteel verdiep ik me in </p>
+        </div>
+
+        {/* 4e regel */}
+        <div>
+          <p>
+            Front-end Development
+            <DevelopIcon
+              aria-hidden
+              focusable="false"
+              className="inline-block align-middle  text-[#4CAF78] "
+            />
+            en werk ik graag aan creatieve
+          </p>
+        </div>
+        {/* 5e regel */}
+        <div>
+          concepten
+          <LampIcon
+            aria-hidden
+            focusable="false"
+            className="inline-block align-middle mx-1 text-[#FFD166]"
+          />
+          die uitgroeien tot sterke digitale oplossingen.
+        </div>
+      </div>
     </section>
   );
 }
