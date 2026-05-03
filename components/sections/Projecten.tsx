87ff473c67aa30e59cc2695ece8dee7099a455b0
ouassilabenhammou <509114@student.fontys.nl>
2026-05-03 18:33:01 +0200

    feat: voeg gridlayout toe aan mijn werk

--- components/sections/Projecten.tsx
+++ components/sections/Projecten.tsx
@@ -5,39 +5,41 @@ export default function Projecten() {
   return (
     <section>
       <SectieTitel title="Mijn Werk" subtitle="Geselecteerde projecten" />
-      <ProjectCard
-        title="Interactief contactformulier"
-        description="Interactieve contactervaring ontworpen voor LiveWall, waarbij een traditioneel formulier is omgezet naar een gamified user experience."
-        tags={["Next.js", "Tailwind CSS", "Three.js"]}
-        year={2026}
-        imageUrl="/images/projecten/livewall-gameshow.png"
-        alt="livewall-gameshow"
-      />
-      <ProjectCard
-        title="Css art"
-        description="CSS art visuals uitgewerkt als experimenteel project, gericht op het verkennen van de creatieve mogelijkheden van code."
-        tags={["Svelte"]}
-        year={2025}
-        imageUrl="/images/projecten/css-art.png"
-        alt="css-art"
-      />
-      <ProjectCard
-        title="KNMI App landingpage"
-        description="Promotionele website ontwikkeld voor de open-source KNMI-app (2ManyDots), gericht op meer bekendheid en downloads."
-        tags={["Next.js", "Tailwind CSS"]}
-        year={2025}
-        imageUrl="/images/projecten/knmi-landingpage.png"
-        alt="knmi-app landingpage"
-      />
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
+        <ProjectCard
+          title="Interactief contactformulier"
+          description="Interactieve contactervaring ontworpen voor LiveWall, waarbij een traditioneel formulier is omgezet naar een gamified user experience."
+          tags={["Next.js", "Tailwind CSS", "Three.js"]}
+          year={2026}
+          imageUrl="/images/projecten/livewall-gameshow.png"
+          alt="livewall-gameshow"
+        />
+        <ProjectCard
+          title="Css art"
+          description="CSS art visuals uitgewerkt als experimenteel project, gericht op het verkennen van de creatieve mogelijkheden van code."
+          tags={["Svelte"]}
+          year={2025}
+          imageUrl="/images/projecten/css-art.png"
+          alt="css-art"
+        />
+        <ProjectCard
+          title="KNMI App landingpage"
+          description="Promotionele website ontwikkeld voor de open-source KNMI-app (2ManyDots), gericht op meer bekendheid en downloads."
+          tags={["Next.js", "Tailwind CSS"]}
+          year={2025}
+          imageUrl="/images/projecten/knmi-landingpage.png"
+          alt="knmi-app landingpage"
+        />
 
-      <ProjectCard
-        title="Generative AI"
-        description="3D-scène gemaakt als experimenteel project, gericht op het verkennen van AI en 3D-modellering en animatie."
-        tags={["AI Tools", "3D Modeling", "Blender"]}
-        year={2024}
-        imageUrl="/images/projecten/generative-ai.png"
-        alt="generative-ai"
-      />
+        <ProjectCard
+          title="Generative AI"
+          description="3D-scène gemaakt als experimenteel project, gericht op het verkennen van AI en 3D-modellering en animatie."
+          tags={["AI Tools", "3D Modeling", "Blender"]}
+          year={2024}
+          imageUrl="/images/projecten/generative-ai.png"
+          alt="generative-ai"
+        />
+      </div>
     </section>
   );
 }
